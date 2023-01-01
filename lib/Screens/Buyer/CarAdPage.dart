6cbc69e7a19307997d978609dbf692324cd3a73f
Omar0Tarek <omar.tarek.elabsawy@gmail.com>
2023-01-01 19:00:27 +0200

    Display car images in CarAdPage

--- lib/Screens/Buyer/CarAdPage.dart
+++ lib/Screens/Buyer/CarAdPage.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:mazadcar/Models/car.dart';
 
 class CarAdPage extends StatefulWidget {
@@ -9,12 +12,53 @@ class CarAdPage extends StatefulWidget {
 }
 
 class _CarAdPageState extends State<CarAdPage> {
-  late Car x;
-
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, Car>;
+    Car car = routeArgs['car']!;
+
     return Scaffold(
-      appBar: AppBar(),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Container(
+        margin: EdgeInsets.only(top: MediaQuery.of(context).viewPadding.top),
+        child: CarouselSlider(
+          options: CarouselOptions(
+            // height: 300.0,
+            aspectRatio: 16 / 9,
+            viewportFraction: 1,
+            initialPage: 0,
+            enableInfiniteScroll: false,
+            reverse: false,
+            autoPlay: true,
+            autoPlayInterval: Duration(seconds: 3),
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enlargeCenterPage: true,
+            enlargeFactor: 0.3,
+            scrollDirection: Axis.horizontal,
+          ),
+          items: [1, 2, 3, 4, 5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  // margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(color: Colors.amber),
+                  child: Image.network(
+                    jsonDecode(car.imageURL)[0],
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
