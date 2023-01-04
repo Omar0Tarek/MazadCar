9327e0c01641f9e913c313c15b28af5c326a0bcd
Omar0Tarek <omar.tarek.elabsawy@gmail.com>
2023-01-04 03:02:07 +0200

    Implement BidsPlaced to show Bids placed by the user

--- lib/Models/userModel.dart
+++ lib/Models/userModel.dart
@@ -4,6 +4,7 @@ class UserModel {
   String phone;
   String name;
   String profilepic;
+  Map<String, int> carAdsWithBids = {};
 
   UserModel(
       {required this.id,
@@ -11,6 +12,14 @@ class UserModel {
       required this.phone,
       required this.profilepic,
       required this.name});
+
+  removeCarAdBid(String carAdId) {
+    carAdsWithBids.remove(carAdId);
+  }
+
+  addCarAdBid(String carAdId, int bidValue) {
+    carAdsWithBids[carAdId] = bidValue;
+  }
 
   static UserModel constructFromFirebase(
       Map<dynamic, dynamic> data, String id) {
