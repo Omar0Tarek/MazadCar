3ca8b65e10034222021e2136f00c42090042d0cc
Omar0Tarek <omar.tarek.elabsawy@gmail.com>
2023-01-05 03:00:37 +0200

    Fix fetching user from db

--- lib/Models/userModel.dart
+++ lib/Models/userModel.dart
@@ -20,10 +20,8 @@ class UserModel {
       name: data['name'] ?? "",
       email: data['email'] ?? "",
       phone: data['phone'] ?? "",
-      profilepic: data['profilepic'] != ""
-          ? data['profilepic']
-          : "https://firebasestorage.googleapis.com/v0/b/mazadcar-60190.appspot.com/o/posts%2Fdata%2Fuser%2F0%2Fcom.example.mazadcarapp%2Fcache%2Fe70822e6-c95f-4ee0-b095-708f7149512d6623604385751819593.jpg?alt=media&token=373fb680-48ac-4f82-91e1-a41e24d629d3" ??
-              "https://firebasestorage.googleapis.com/v0/b/mazadcar-60190.appspot.com/o/posts%2Fdata%2Fuser%2F0%2Fcom.example.mazadcarapp%2Fcache%2Fe70822e6-c95f-4ee0-b095-708f7149512d6623604385751819593.jpg?alt=media&token=373fb680-48ac-4f82-91e1-a41e24d629d3",
+      profilepic: data['profilepic'] ??
+          "https://firebasestorage.googleapis.com/v0/b/mazadcar-60190.appspot.com/o/posts%2Fdata%2Fuser%2F0%2Fcom.example.mazadcarapp%2Fcache%2Fe70822e6-c95f-4ee0-b095-708f7149512d6623604385751819593.jpg?alt=media&token=373fb680-48ac-4f82-91e1-a41e24d629d3",
     );
   }
 }
